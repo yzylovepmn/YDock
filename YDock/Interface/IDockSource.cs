f0eccf63045653a387f1710ea8a671588a0a5efa
哲宇 杨 <7836596@qq.com>
2018-06-21 22:09:59 +0800

    6-21

---
+++ YDock/Interface/IDockSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace YDock.Interface
+{
+    public interface IDockSource
+    {
+        string Header { get; }
+        ImageSource Icon { get; }
+        IDockControl DockControl { get; set; }
+    }
+
+    public interface IDockDocSource : IDockSource
+    {
+        bool AllowClose();
+        string FullFileName { get; }
+    }
+}
